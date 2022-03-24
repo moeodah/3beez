4b4d6ad2493fb168684f1978db8ed1977bf38880
Mohammad <moeodah@gmail.com>
2022-03-24 16:09:01 -0400

    initial

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const express = require ('express')
-const servicStatic = require('serve-static')
-const path = require('path')
-const serveStatic = require('serve-static')
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 4000
 
 const app = express()
 
-app.use('/',serveStatic(path.join(__dirname,'/dist')))
-
-app.get(/.*/,function(res,req){
-  res.sendFile(path.join(__dirname,'/dist/index.html'))
-})
-const port = process.env.PORT || 5000
+app.use(history())
+app.use(serveStatic(__dirname + '/dist/spa'))
 app.listen(port)
-
-console.log("listening on port : "+port)
